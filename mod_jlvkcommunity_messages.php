5df14a73a10c8bb303d36499cc727db48e4fbd4f
zikkuratvk <zikkuratvk@gmail.com>
2016-12-18 18:30:08 +0200

    версия 1.1 Добавление новых функций виджета вконтакте.

--- mod_jlvkcommunity_messages.php
+++ mod_jlvkcommunity_messages.php
@@ -12,9 +12,41 @@
 defined('_JEXEC') or die('Restricted access');
 
 require_once dirname(__FILE__).'/helper.php';
-$group_id 	= $params->get('group_id');
-$mode 		= $params->get('mode');
-$link 		= $params->get('link');
-$linknone	= '';
 
+$group_id 					= $params->get('group_id');
+$mode 						= $params->get('mode');
+$link 						= $params->get('link');
+$linknone					= '';
+$disableButtonTooltip 		= $params->get('disableButtonTooltip');
+$tooltipButtonText 			= $params->get('tooltipButtonText');
+$disableNewMessagesSound	= $params->get('disableNewMessagesSound');
+$disableExpandChatSound 	= $params->get('disableExpandChatSound');
+$widgetPosition 			= $params->get('widgetPosition');
+$expanded 					= $params->get('expanded');
+
+
+if ($link==0){
+	$linknone = '<div style="text-align: right;"><a href="http://afisha-msk.ru/" target="_blank" style="text-decoration:none; color: #c0c0c0; font-family: arial,helvetica,sans-serif; font-size: 5pt; ">afisha-msk.ru</a></div>';
+	}
+
+if ($expanded==0){
+	$expandedon = '';
+	}
+else {
+	$expandedon = ('expandTimeout:'.$expanded.',');
+	}	
+
+if ($disableButtonTooltip==0){
+	$disableButtonTooltipon = 'disableButtonTooltip: "1",';
+	}
+	
+if ($disableNewMessagesSound==0){
+	$disableNewMessagesSoundon = 'disableNewMessagesSound: "1",';
+	}
+
+	
+if ($disableExpandChatSound==0){
+	$disableExpandChatSoundon = 'disableExpandChatSound: "1",';
+	}
+	
 require JModuleHelper::getLayoutPath('mod_jlvkcommunity_messages', $params->get('layout', 'default'));
--- tmpl/default.php
+++ tmpl/default.php
@@ -9,22 +9,13 @@
 */
 
 $doc = JFactory::getDocument();
-$doc->addScript("//vk.com/js/api/openapi.js?127");
-if ($link==0){
-	$linknone = '<div style="text-align: right;"><a href="http://afisha-msk.ru/" target="_blank" style="text-decoration:none; color: #c0c0c0; font-family: arial,helvetica,sans-serif; font-size: 5pt; ">afisha-msk.ru</a></div>';
-	}
-else {}
-
-if ($mode==0){
-	$modeon = 'shown: "1"';
-	}
-else {}
+$doc->addScript("//vk.com/js/api/openapi.js?136");
 ?>
 
 <div id="jlvk_community_messages<?php echo $group_id;?>">
 <?php echo $linknone;?>
 </div>
 <script type="text/javascript">
-VK.Widgets.CommunityMessages("jlvk_community_messages<?php echo $group_id;?>", <?php echo $group_id;?>, {<?php echo $modeon;?>});
+VK.Widgets.CommunityMessages("jlvk_community_messages<?php echo $group_id;?>", <?php echo $group_id;?>, {<?php echo $expandedon;?>widgetPosition: "<?php echo $widgetPosition;?>",<?php echo $disableNewMessagesSoundon;?><?php echo $disableExpandChatSoundon;?><?php echo $disableButtonTooltipon;?> tooltipButtonText: "<?php echo $tooltipButtonText;?>",buttonType: "blue_circle"});
 </script>
 
